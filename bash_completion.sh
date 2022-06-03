512bd3af468bc0ed14f314d1d92d119db5a87c4a
Daniel <dnlmrls9@gmail.com>
2022-06-03 18:16:22 +0200

    simplified completion

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,45 +1,52 @@
 # BASH
 COMPLETION_DIR="$(brew --prefix)/etc/bash_completion.d"
-GITHUB_URL="https://raw.githubusercontent.com/git/git/master/contrib/completion"
+
+function get_github_completion() {
+	github_url="https://raw.githubusercontent.com/git/git/master/contrib/completion"
+
+	if [[ ! -f "${COMPLETION_DIR}/$1" ]] ;
+	then
+    	wget -O "${COMPLETION_DIR}/$1" "${github_url}/$1"
+	fi
+}
+
+function get_docker_completion() {
+	docker_etc="/Applications/Docker.app/Contents/Resources/etc"
+	if [[ -d "${COMPLETION_DIR}" && ! -L "${COMPLETION_DIR}/$1" ]] ;
+	then
+  		ln -s "${docker_etc}/$1" "${COMPLETION_DIR}/$1"
+	fi
+}
 
 # PREP WORK
 # git completion preparation
 _git_prompt="git-prompt.sh"
 _git_completion="git-completion.bash"
 
-if [[ ! -f "${COMPLETION_DIR}/${_git_completion}" ]] ;
-then
-    wget -O "${COMPLETION_DIR}/${_git_completion}" "${GITHUB_URL}/${_git_completion}"
-fi
+get_github_completion $_git_prompt
 
-if [[ ! -f "${COMPLETION_DIR}/${_git_prompt}" ]] ;
-then
-    wget -O "${COMPLETION_DIR}/${_git_prompt}" "${GITHUB_URL}/${_git_prompt}"
-fi
+get_github_completion $_git_completion
+
 
 # docker completion preparation
-docker_etc="/Applications/Docker.app/Contents/Resources/etc"
-if [[ -d "${COMPLETION_DIR}" && ! -L "${COMPLETION_DIR}/docker" ]] ;
-then
-  ln -s "${docker_etc}/docker.bash-completion" "${COMPLETION_DIR}/docker"
-fi
+_docker="docker.bash-completion"
+_docker_compose="docker-compose.bash-completion"
 
-if [[ -d "${COMPLETION_DIR}" && ! -L "${COMPLETION_DIR}/docker-compose" ]] ;
-then
-  ln -s "${docker_etc}/docker-compose.bash-completion" "${COMPLETION_DIR}/docker-compose"
-fi
+get_docker_completion $_docker
 
-# SOURCING COMPLETION
+get_docker_completion $_docker_compose
 
+
+# SOURCE COMPLETION
 # git completion
 source "${COMPLETION_DIR}/${_git_completion}"
 # git prompt
 source "${COMPLETION_DIR}/${_git_prompt}"
 
 # docker completion
-source "${COMPLETION_DIR}/docker"
+source "${COMPLETION_DIR}/${_docker}"
 # docker-compose completion
-source "${COMPLETION_DIR}/docker-compose"
+source "${COMPLETION_DIR}/${_docker-compose}"
 
 # kubectl completion
 source "${COMPLETION_DIR}/kubectl"
