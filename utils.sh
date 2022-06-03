b2f07ea3c465c125d159a76826eec6b2aac10d60
Daniel <dnlmrls9@gmail.com>
2022-06-03 15:24:15 +0200

    bash_profile link

--- utils.sh
+++ utils.sh
@@ -67,8 +67,7 @@ function change_shell() {
 
 function link_bash_file() {
     _bash_file=$(get_bash_file)
-    # if file not exists
-    ln -sfn "$HOME/dotfiles/.bash_profile" "$HOME/$_bash_file"
-    echo "${_bash_file}"
+    ln -sf "$HOME/dotfiles/.bash_profile" "$HOME/$_bash_file"
+    echo "$_bash_file"
 }
 
