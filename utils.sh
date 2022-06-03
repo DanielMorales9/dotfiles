8cfb509743de2f6f6b5d3d221be76100fa8fef74
Daniel <dnlmrls9@gmail.com>
2022-06-03 14:51:18 +0200

    refactoring bootstrap

--- utils.sh
+++ utils.sh
@@ -68,7 +68,7 @@ function change_shell() {
 function link_bash_file() {
     _bash_file=$(get_bash_file)
     # if file not exists
-    ln -sf "$HOME/dotfiles/.bash_profile" "$HOME/$_bash_file"
+    [[ ! -f  "$HOME/$_bash_file" ]] && ln -sf "$HOME/dotfiles/.bash_profile" "$HOME/$_bash_file"
     echo "${_bash_file}"
 }
 
