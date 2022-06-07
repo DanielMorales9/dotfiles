887892df7030f0cf2410d99c3b5093f44696d37f
Daniel <dnlmrls9@gmail.com>
2022-06-07 17:15:19 +0200

    sourcing bash_file

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,15 @@ else
 	exit 1
 fi
 
-change_shell $DESIRED_SHELL
+change_shell "$DESIRED_SHELL"
 
 install_packages
 
 install_apps
 
 setup_direnv
+
+_bash_file="$(link_bash_file)"
+
+# shellcheck source=/dev/null
+source "$HOME/${_bash_file}"
