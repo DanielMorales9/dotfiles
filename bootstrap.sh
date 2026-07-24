f78c2506ce5813b562d9dfabdbde2051b4bd2ef0
Daniel Morales <dnlmrls9@gmail.com>
2026-07-24 11:27:29 +0200

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@ set -ex
 
 DESIRED_SHELL="/bin/bash"
 
-source "$(pwd)/utils.sh"
+source "$HOME/dotfiles/utils.sh"
 
 change_shell "$DESIRED_SHELL"
 
 OS_CODE=$(get_os_code)
 if [[ $OS_CODE -eq 1 ]]; then
-	source "$(pwd)/osx/bootstrap.sh"
+	source "$HOME/dotfiles/osx/bootstrap.sh"
 else
 	echo "Bootstrap not possible on this OS"
 	exit 1
